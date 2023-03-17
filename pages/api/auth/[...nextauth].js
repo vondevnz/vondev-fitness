f04a015acb03c252764b43ffc9255d1e2c6c7763
vondevnz <fergus@vondev.co.nz>
2023-03-17 18:53:14 +1300

    added navbar + login button

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,10 +5,12 @@ import prisma from "../../../prisma/client"
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      checks: "state"
     }),
   ],
 })
